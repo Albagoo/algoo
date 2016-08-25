1765eed910065215938d8533f11f35dd0ec05b6e
User <User@UserPC71>
2016-08-25 19:46:07 +0900

    업로드 - 신정

--- algoo/src/main/java/com/algoo/app/notice/model/NoticeDAO.java
+++
@@ -1,5 +0,0 @@
-package com.algoo.app.notice.model;
-
-public interface NoticeDAO {
-	public int insertNotice(NoticeVO vo);
-}
--- algoo/src/main/java/com/algoo/app/notice/model/NoticeService.java
+++
@@ -1,5 +0,0 @@
-package com.algoo.app.notice.model;
-
-public interface NoticeService {
-	public int insertNotice(NoticeVO vo);
-}

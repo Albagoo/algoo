c02574ee589b142b591784c0d13254fa4fa21b07
User <User@UserPC67>
2016-08-29 08:43:18 +0900

    에러 수정

--- algoo/src/main/java/com/algoo/app/notice/model/NoticeServiceImpl.java
+++ algoo/src/main/java/com/algoo/app/notice/model/NoticeServiceImpl.java
@@ -3,7 +3,9 @@ package com.algoo.app.notice.model;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NoticeServiceImpl implements NoticeService{
 	@Autowired
 	private NoticeDAO noticeDao;

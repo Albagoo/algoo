fc809b84aa42846284ba7295dc110f121c1d021d
User <User@UserPC71>
2016-09-02 14:29:02 +0900

    공지사항 VO의 누락된 부분 보충 16-09-02 by 김신정

--- algoo/src/main/java/com/algoo/app/notice/model/NoticeVO.java
+++ algoo/src/main/java/com/algoo/app/notice/model/NoticeVO.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 public class NoticeVO {
 	private int mainNo;
+	private String category;
 	private String title;
 	private String content;
 	private String writer;
@@ -14,63 +15,75 @@ public class NoticeVO {
 	public NoticeVO() {
 		super();
 	}
-	public NoticeVO(int mainNo, String title, String content, String writer, int readCount, Timestamp regdate,
-			int authcode) {
-		super();
-		this.mainNo = mainNo;
-		this.title = title;
-		this.content = content;
-		this.writer = writer;
-		this.readCount = readCount;
-		this.regdate = regdate;
-		this.authcode = authcode;
-	}
+
 	public int getMainNo() {
 		return mainNo;
 	}
+
 	public void setMainNo(int mainNo) {
 		this.mainNo = mainNo;
 	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public String getWriter() {
 		return writer;
 	}
+
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
+
 	public int getReadCount() {
 		return readCount;
 	}
+
 	public void setReadCount(int readCount) {
 		this.readCount = readCount;
 	}
+
 	public Timestamp getRegdate() {
 		return regdate;
 	}
+
 	public void setRegdate(Timestamp regdate) {
 		this.regdate = regdate;
 	}
+
 	public int getAuthcode() {
 		return authcode;
 	}
+
 	public void setAuthcode(int authcode) {
 		this.authcode = authcode;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "NoticeVO [mainNo=" + mainNo + ", title=" + title + ", content=" + content + ", writer=" + writer
-				+ ", readCount=" + readCount + ", regdate=" + regdate + ", authcode=" + authcode + "]";
+		return "NoticeVO [mainNo=" + mainNo + ", category=" + category + ", title=" + title + ", content=" + content
+				+ ", writer=" + writer + ", readCount=" + readCount + ", regdate=" + regdate + ", authcode=" + authcode
+				+ "]";
 	}
 }

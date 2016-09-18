99919cfd720fc6385fdc0abb8effc653e9fb6cb4
User <User@UserPC63>
2016-09-18 20:21:52 +0900

    채용공고 매퍼XML 채용공고서치VO 업데이트 대근

--- algoo/src/main/java/com/algoo/app/rec/model/RecSeachVO.java
+++ algoo/src/main/java/com/algoo/app/rec/model/RecSeachVO.java
@@ -2,11 +2,58 @@ package com.algoo.app.rec.model;
 
 import com.algoo.app.common.SearchVO;
 
-public class RecSeachVO extends SearchVO{
+public class RecSeachVO extends SearchVO {
+
+	private String area1;
+	private String area2;
+	private String area3;
+	private String area4;
+	private String area5;
+
+	public String getArea1() {
+		return area1;
+	}
+
+	public void setArea1(String area1) {
+		this.area1 = area1;
+	}
+
+	public String getArea2() {
+		return area2;
+	}
+
+	public void setArea2(String area2) {
+		this.area2 = area2;
+	}
+
+	public String getArea3() {
+		return area3;
+	}
+
+	public void setArea3(String area3) {
+		this.area3 = area3;
+	}
+
+	public String getArea4() {
+		return area4;
+	}
+
+	public void setArea4(String area4) {
+		this.area4 = area4;
+	}
+
+	public String getArea5() {
+		return area5;
+	}
+
+	public void setArea5(String area5) {
+		this.area5 = area5;
+	}
 
 	@Override
 	public String toString() {
-		return "RecSeachVO [toString()=" + super.toString() + "]";
+		return "RecSeachVO [area1=" + area1 + ", area2=" + area2 + ", area3=" + area3 + ", area4=" + area4 + ", area5="
+				+ area5 + ", toString()=" + super.toString() + "]";
 	}
 
 }

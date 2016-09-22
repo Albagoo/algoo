e9d851285480267e6d4f06bec030a3e0cfa26d7e
User <User@UserPC71>
2016-09-22 17:31:44 +0900

    이용약관 추가 by신정

---
+++ algoo/src/main/java/com/algoo/app/provision/ProvisionController.java
@@ -0,0 +1,22 @@
+package com.algoo.app.provision;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.algoo.app.member.controller.LoginController;
+
+@Controller
+@RequestMapping("/provision")
+public class ProvisionController {
+	private static final Logger logger=LoggerFactory.getLogger(LoginController.class);
+	
+	@RequestMapping("/cProvision.ag")
+	public String CProv(){
+		//1.
+		logger.info("기업회원약관 보여주기");
+		
+		return "provision/cprovision";
+	}
+}

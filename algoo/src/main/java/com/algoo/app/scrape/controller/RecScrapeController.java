ce4f66ed2c90cfc3bc189ce1d28f09da64837b5e
User <User@UserPC63>
2016-09-26 08:55:49 +0900

    임시저장 대근

--- algoo/src/main/java/com/algoo/app/scrape/controller/RecScrapeController.java
+++ algoo/src/main/java/com/algoo/app/scrape/controller/RecScrapeController.java
@@ -1,13 +1,22 @@
 package com.algoo.app.scrape.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.algoo.app.scrape.model.RecScrapeService;
+import com.algoo.app.scrape.model.RecScrapeVO;
 
 @Controller
+@RequestMapping("/recScrape")
 public class RecScrapeController {
 
 	@Autowired
@@ -16,5 +25,18 @@ public class RecScrapeController {
 	Logger logger
 	=LoggerFactory.getLogger(RecScrapeController.class);
 	
-	
+	@RequestMapping(value="/addScrape.ag")
+	public String Scrapeadd(
+			@ModelAttribute RecScrapeVO recScrapeVo,
+			HttpSession session,
+			Model model){
+		String userid = (String) session.getAttribute("userid");
+
+		
+		/*List<RecScrapeVO> alist = rsService.selectRecScrape(recScrapeVo);*/
+		
+		return null;
+		
+		
+	}
 }
